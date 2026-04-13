bbba7bbb959cceebeaa7cdba0fdcf3098a867e56
sanaaa1906 <sanaaajadhav@gmail.com>
2026-04-13 15:38:04 +0530

    Update app.py

--- SymbiPlan/app.py
+++ SymbiPlan/app.py
@@ -118,7 +118,7 @@ if st.session_state.page == 'Home':
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         # Just use the filename. Streamlit looks in your GitHub folder automatically.
-        st.image("logo.jpg", use_container_width=True) 
+        st.image("SymbiPlan/logo.jpg", use_container_width=True) 
     
     st.markdown("<h1 style='text-align: center; color: #1E3A8A; font-family: sans-serif;'>SymbiPlan</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; font-style: italic;'>Your Campus, Your Signal, Your Solution.</p>", unsafe_allow_html=True)
